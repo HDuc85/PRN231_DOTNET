bcdf6a755704ce2cfcf0f88d16218576497f301b
Toan Nang <112840020+Le-Nguyen-Toan-Nang@users.noreply.github.com>
2024-05-27 17:13:15 +0700

    MVC WebApp: base view

---
+++ Source/Net1711_231_5_InternManagement/InternManagementWebApp/Controllers/InternProfilesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using InternManagementData.Models;
+using Newtonsoft.Json;
+
+namespace InternManagementWebApp.Controllers
+{
+    public class InternProfilesController : Controller
+    {
+        private string apiUrl = "https://localhost:7038/api/Intern/";
+        public InternProfilesController()
+        {
+        }
+        public IActionResult index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<List<InternProfile>> GetAll()
+        {
+            try
+            {
+                var result = new List<InternProfile>();
+                using (var httpClient = new HttpClient())
+                {
+                    using (var response = await httpClient.GetAsync(apiUrl + "GetAll"))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var content = await response.Content.ReadAsStringAsync();
+                            result = JsonConvert.DeserializeObject<List<InternProfile>>(content);
+                        }
+                    }
+                }
+                return result;
+            } catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
